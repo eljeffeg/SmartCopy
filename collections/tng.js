5804a1d35fca9c11f80f9727172f284b7ea5bab2
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-01-29 22:20:55 +0100

    TNG: process parents

--- collections/tng.js
+++ collections/tng.js
@@ -8,7 +8,7 @@ registerCollection({
   "loadPage": function(request) {
     console.log("Loading page");
     var parsed = $(request.source.replace(/<img[^>]*>/ig, ""));
-    focusname = getTNGField(parsed, "Name");
+    focusname = getTNGName(parsed);
     recordtype = "TNG Genealogy";
   },
   "parseProfileData": parseTNG
@@ -20,9 +20,23 @@ function getTNGField(parsed, name, position) {
   for (i = 0; i<position; i++) {
     selector += "+ td";
   }
-  var elem = parsed.find(selector);
+  return parsed.find(selector);
+}
+
+function getTNGFieldText(parsed, name, position) {
+  var elem = getTNGField(parsed, name, position);
   if (exists(elem)) {
     return elem.text().trim();
+  }
+  return "";
+}
+
+function getTNGName(parsed) {
+  var elem = getTNGField(parsed);
+  if (exists(elem)) {
+    var familyname = elem.find(".family-name").text();
+    var givenname = elem.find(".given-name").text();
+    return givenname + " " + familyname;
   }
   return "";
 }
@@ -31,8 +45,8 @@ function parseTNG(htmlstring, familymembers, relation) {
   relation = relation || "";
   var parsed = $(htmlstring);
 
-  focusperson = getTNGField(parsed, "Name");
-  genderval = getTNGField(parsed, "Gender");
+  focusperson = getTNGFieldText(parsed, "Name");
+  genderval = getTNGFieldText(parsed, "Gender");
   focusname = focusperson;
 
   document.getElementById("readstatus").innerHTML = escapeHtml(focusperson);
@@ -41,6 +55,21 @@ function parseTNG(htmlstring, familymembers, relation) {
 
   profiledata["birth"] = parseTNGDate(parsed, "Born");
   profiledata["death"] = parseTNGDate(parsed, "Died");
+
+  if (familymembers) {
+    loadGeniData();
+    var famid = 0;
+
+    var father = getTNGField(parsed, "Father");
+    if (exists(father)) {
+      processTNGFamily(father, "father", famid);
+    }
+
+    var mother = getTNGField(parsed, "Mother");
+    if (exists(mother)) {
+      processTNGFamily(mother, "mother", famid);
+    }
+  }
 
   if (familymembers) {
     alldata["profile"] = profiledata;
@@ -53,14 +82,78 @@ function parseTNG(htmlstring, familymembers, relation) {
 
 function parseTNGDate(parsed, name) {
   var data = [];
-  var dateval = cleanDate(getTNGField(parsed, name, 1));
+  var dateval = cleanDate(getTNGFieldText(parsed, name, 1));
   if (dateval !== "") {
     data.push({date: dateval});
   }
-  var eventlocation = getTNGField(parsed, name, 2);
+  var eventlocation = getTNGFieldText(parsed, name, 2);
   if (eventlocation !== "") {
     data.push({id: geoid, location: eventlocation});
     geoid++;
   }
   return data;
+}
+
+function processTNGFamily(person, title, famid) {
+  var url = $(person).find("a").attr("href");
+  if (exists(url)) {
+    if (!exists(alldata["family"][title])) {
+      alldata["family"][title] = [];
+    }
+    var gendersv = "unknown";
+    var name = $(person).find("a").text();
+    var itemid = getTNGItemId(url);
+    // Get base path
+    var basesplit = tablink.split("/");
+    basesplit.pop();
+    var fullurl = basesplit.join("/") + "/" + url;
+    var text = $(person).text();
+    var subdata = {name: name, title: title, gender: gendersv, url: fullurl, itemId: itemid, profile_id: famid};
+    console.log("Person is "+title+" and has subdata");
+    console.log(subdata);
+
+    // Parse marriage data
+    /*
+    if ($(person).text().startsWith("Married")) {
+      var marriageinfo = $(person).find("em").first();
+      if (exists(marriageinfo)) {
+        subdata["marriage"] = parseGeneanetDate(marriageinfo.text());
+        console.log(subdata["marriage"]);
+      }
+    }
+    */
+    unionurls[famid] = itemid;
+    getTNGFamily(famid, fullurl, subdata);
+  }
+}
+
+function getTNGFamily(famid, url, subdata) {
+    familystatus.push(famid);
+    chrome.extension.sendMessage({
+        method: "GET",
+        action: "xhttp",
+        variable: subdata,
+        url: url
+    }, function (response) {
+        var arg = response.variable;
+        var person = parseTNG(response.source, false, {"title": arg.title, "proid": arg.profile_id, "itemId": arg.itemId});
+        if (person === "") {
+            familystatus.pop();
+            return;
+        }
+        person = updateInfoData(person, arg);
+        databyid[arg.profile_id] = person;
+        alldata["family"][arg.title].push(person);
+        familystatus.pop();
+    });
+}
+
+function getTNGItemId(url) {
+    if (exists(url)) {
+        var p = getParameterByName("personID", url);
+        var t = getParameterByName("tree", url);
+        return "personID="+p+"&tree="+t;
+    } else {
+        return "";
+    }
 }

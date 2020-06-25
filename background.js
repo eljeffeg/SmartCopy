22d31e6c2440dee98560cdd8819ba77b8d7bdbb4
ElJefe <jeffgemail@gmail.com>
2020-06-25 13:01:19 -0400

    Fix x-frame origin issue

--- background.js
+++ background.js
@@ -42,3 +42,20 @@ function exists(object) {
     return (typeof object !== "undefined" && object !== null);
 }
 
+chrome.webRequest.onHeadersReceived.addListener(
+    function(info) {
+        var headers = info.responseHeaders;
+        for (var i=headers.length-1; i>=0; --i) {
+            var header = headers[i].name.toLowerCase();
+            if (header == 'x-frame-options' || header == 'frame-options') {
+                headers.splice(i, 1); // Remove x-frame header origin policy
+            }
+        }
+        return {responseHeaders: headers};
+    },
+    {
+        urls: [ 'https://www.geni.com/platform/oauth/*' ],
+        types: [ 'sub_frame' ]
+    },
+    ['blocking', 'responseHeaders']
+);
